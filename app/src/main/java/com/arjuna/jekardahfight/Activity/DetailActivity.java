d716e344587132caa7c8aa064738dc3e82f3132c
Arjuna Pratama <32661531+ArjunaPratama@users.noreply.github.com>
2018-09-07 15:05:20 +0700

    Make a Detail Activity

---
+++ app/src/main/java/com/arjuna/jekardahfight/Activity/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.arjuna.jekardahfight.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arjuna.jekardahfight.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
